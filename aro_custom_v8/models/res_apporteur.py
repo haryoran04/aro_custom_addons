777ec7f6ef5c3322931e3cf082a3f29d1b8e8590
aroodoo <aroodoo2017@gmail.com>
2017-02-12 09:40:58 +0300

    add model res.apporteur

---
+++ aro_custom_v8/models/res_apporteur.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, api, fields, exceptions
+
+
+class ResApporteur(models.Model):
+    _name = 'res.apporteur'
+    _inherits = {'res.partner': 'partner_id'}
+    _description = ''
+
+    partner_id = fields.Many2one('res.partner', string='Related Partner',
+                                 required=True,
+                                 ondelete='restrict', auto_join=True,
+                                 help='Partner-related data of apporteur')
+    name = fields.Char(related='partner_id.name', inherited=True)
+    agency_id = fields.Many2one(related='partner_id.agency_id', inherited=True)
+    title = fields.Many2one(related='partner_id.title', inherited=True)
+    ap_code = fields.Char(string='AP code', size=8)
